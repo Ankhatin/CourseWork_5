b714bdb68a916fb33e329eb329a8bfaac00d3820
Andrey Khatin <ankhatin@gmail.com>
2024-07-01 19:46:43 +0700

    create webServicesAPI module

---
+++ src/webServicesAPI.py
@@ -0,0 +1,33 @@
+import requests
+from abc import ABC, abstractmethod
+
+
+class Parser(ABC):
+
+    @abstractmethod
+    def load_data(self, keyword):
+        pass
+
+
+class HeadHunterAPI(Parser):
+    '''
+    Класс для работы с вакансиями, загруженными с платформы hh.ru
+    '''
+    def __init__(self, url):
+        self.url = url
+        self.headers = {'User-Agent': 'HH-User-Agent'}
+        self.params = {'text': '', 'only_with_vacancies': True, 'only_with_salary': True, 'page': 0, 'per_page': 10}
+
+    def load_data(self, keyword=None, per_page=10):
+        '''
+        Функция отправляет запрос на платформу hh.ru,
+        загружает данные и возращает в виде список словарей
+        '''
+        employers_list: list[dict] = []
+        self.params['text'] = keyword
+        self.params['per_page'] = per_page
+        response = requests.get(self.url, headers=self.headers, params=self.params)
+        if response.status_code == 200:
+            return response.json()['items']
+
+
